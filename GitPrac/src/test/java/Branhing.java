82688db141376170c1c42ca6e86eaa5e13eaaaa0
SHAIK NASRUDDIN <SHAIK NASRUDDIN@192.168.0.101>
2022-04-10 10:33:15 +0530

    changes are made

--- GitPrac/src/test/java/Branhing.java
+++ GitPrac/src/test/java/Branhing.java
@@ -4,6 +4,7 @@ public class Branhing {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		//comments are added i the project 
 		System.out.println("push the code to new branch");
 		
 	}
